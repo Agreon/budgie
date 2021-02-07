8a4f9b7d3da9d5207923cf1d057a4f7d203e3640
bexrr <72194279+bexrr@users.noreply.github.com>
2021-02-07 11:12:51 +0100

    add get single expense route, add status codes in case of errors (#26)
    Co-authored-by: bexrr <no-mail>

--- server/routes.go
+++ server/routes.go
@@ -56,6 +56,36 @@ func listExpenses(c *gin.Context) {
 	//})
 }
 
+func listSingleExpense(c *gin.Context) {
+	tokenInput := c.GetHeader("token")
+
+	userID, tokenIsValid := checkTokenIsValid(tokenInput)
+
+	if tokenIsValid {
+		expenseID := c.Param("id")
+
+		db := GetDB()
+		expense := Expense{}
+		err := db.Get(&expense, "SELECT * FROM expense WHERE id=$1", expenseID)
+
+		log.Println(expenseID)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(400)
+			return
+		}
+
+		if expense.UserID != userID {
+			c.AbortWithStatus(403)
+			return
+		}
+
+		c.JSON(200, expense)
+	} else {
+		c.AbortWithStatus(401)
+	}
+}
+
 func login(c *gin.Context) {
 
 	userNameInput := c.Query("name")
