417fdd76ccb845f6cac11142b16fc926abce2374
bexrr <72194279+bexrr@users.noreply.github.com>
2021-02-25 17:30:50 +0100

    add tags to expenses (#65)
    Co-authored-by: bexrr <no-mail>

--- server/tag.go
+++ server/tag.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"errors"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,7 +9,6 @@ import (
 )
 
 var tagTable = `
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS tag (
@@ -36,10 +35,10 @@ func insertTag(c *gin.Context) {
 	var newTag TagInput
 	err := c.BindJSON(&newTag)
 	if err != nil {
+		saveErrorInfo(c, err, 400)
 		return
 	}
 
-	/* get userID from middleware */
 	userID := c.MustGet("userID")
 
 	db := GetDB()
@@ -48,18 +47,14 @@ func insertTag(c *gin.Context) {
 	var tag Tag
 	err = db.Get(&tag, "SELECT * FROM tag WHERE name=$1 AND user_id=$2", newTag.Name, userID)
 	if err == nil {
-		log.Println("Tag already exists.")
-		c.AbortWithStatus(409)
+		saveErrorInfo(c, errors.New("Tag already exists."), 400)
 		return
 	}
 
 	/* insert new tag */
 	_, err = db.Exec("INSERT INTO tag VALUES (uuid_generate_v4(), $1, $2, now(), now())", newTag.Name, userID)
-
-	/* if there is a database error */
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatus(500)
+		saveErrorInfo(c, err, 500)
 		return
 	}
 
@@ -71,14 +66,11 @@ func listTags(c *gin.Context) {
 	db := GetDB()
 	tags := []Tag{}
 
-	/* get userID from middleware */
 	userID := c.MustGet("userID")
 
 	err := db.Select(&tags, "SELECT * FROM tag WHERE user_id=$1 ORDER BY created_at DESC", userID)
-
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatus(500)
+		saveErrorInfo(c, err, 500)
 		return
 	}
 
@@ -94,17 +86,15 @@ func updateTag(c *gin.Context) {
 
 	/* check if tag exists */
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatus(400)
+		saveErrorInfo(c, err, 400)
 		return
 	}
 
-	/* get userID from middleware */
 	userID := c.MustGet("userID")
 
 	/* check if tag belongs to requesting user */
 	if tag.UserID != userID {
-		c.AbortWithStatus(403)
+		saveErrorInfo(c, errors.New("Tag does not belong to this user!"), 403)
 		return
 	}
 
@@ -112,23 +102,19 @@ func updateTag(c *gin.Context) {
 	var updateTag TagInput
 	err = c.BindJSON(&updateTag)
 	if err != nil {
+		saveErrorInfo(c, err, 400)
 		return
 	}
 
 	_, err = db.Exec("UPDATE tag SET name=$1, updated_at=now() WHERE id=$2", updateTag.Name, tagID)
-
-	/* if there is a database error */
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatus(500)
+		saveErrorInfo(c, err, 500)
 		return
 	}
 
 	err = db.Get(&tag, "SELECT * FROM tag WHERE id=$1", tagID)
-
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatus(500)
+		saveErrorInfo(c, err, 500)
 		return
 	}
 
