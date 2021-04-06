ecee1901f772f75c11495f8499bab2bf7a290baa
bexrr <no-mail>
2021-04-06 16:32:28 +0200

    add savings rate

--- server/budgetoverview.go
+++ server/budgetoverview.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ExpenseByCategory struct {
 	Category   string `db:"category" json:"category"`
@@ -16,6 +21,7 @@ type BudgetOverview struct {
 	TotalExpense      string              `json:"total_expense"`
 	ExpenseByCategory []ExpenseByCategory `json:"expense_by_category"`
 	ExpenseByTag      []ExpenseByTag      `json:"expense_by_tag"`
+	SavingsRate       string              `json:"savings_rate"`
 	TotalIncome       string              `json:"total_income"`
 }
 
@@ -29,6 +35,7 @@ func getBudgetOverview(c *gin.Context) {
 		saveErrorInfo(c, err, 500)
 		return
 	}
+	totalExpense, _ := strconv.ParseFloat(budgetOverview.TotalExpense, 64)
 
 	err = db.Select(&budgetOverview.ExpenseByCategory, "SELECT category, COALESCE(SUM(costs),0) AS total FROM expense WHERE user_id=$1 GROUP BY category ORDER BY total", userID)
 	if err != nil {
@@ -48,6 +55,10 @@ func getBudgetOverview(c *gin.Context) {
 		saveErrorInfo(c, err, 500)
 		return
 	}
+	totalIncome, _ := strconv.ParseFloat(budgetOverview.TotalIncome, 64)
+
+	savingsRate := 100 - totalExpense*100/totalIncome
+	budgetOverview.SavingsRate = fmt.Sprintf("%.2f %%", savingsRate)
 
 	c.JSON(200, budgetOverview)
 }
