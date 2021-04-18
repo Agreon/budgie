7c205fa74d9631cf40dbee81002246063791c9fb
bexrr <no-mail>
2021-04-18 09:19:17 +0200

    add percentages of categories

--- server/budgetoverview.go
+++ server/budgetoverview.go
@@ -8,8 +8,10 @@ import (
 )
 
 type ExpenseByCategory struct {
-	Category   string `db:"category" json:"category"`
-	TotalCosts string `db:"total" json:"total_costs"`
+	Category       string `db:"category" json:"category"`
+	PercentageOnce string `db:"percentage_once" json:"percentage_of_non_recurring_expenses"`
+	PercentageAll  string `db:"percentage_all" json:"percentage_of_all_expenses"`
+	TotalCosts     string `db:"total" json:"total_costs"`
 }
 
 type ExpenseByTag struct {
@@ -20,11 +22,15 @@ type ExpenseByTag struct {
 }
 
 type BudgetOverview struct {
-	TotalExpense string `db:"expense_total" json:"total_expense"`
-	//ExpenseByCategory []ExpenseByCategory `json:"expense_by_category"`
-	ExpenseByTag []ExpenseByTag `json:"expense_by_tag"`
-	SavingsRate  string         `db:"savings_rate" json:"savings_rate"`
-	TotalIncome  string         `db:"income_total" json:"total_income"`
+	ExpensesRecurring string              `db:"expense_recurring" json:"total_expense_recurring"`
+	ExpensesOnce      string              `db:"expense_once" json:"total_expense_once"`
+	TotalExpense      string              `db:"expense_total" json:"total_expense"`
+	ExpenseByCategory []ExpenseByCategory `json:"expense_by_category"`
+	ExpenseByTag      []ExpenseByTag      `json:"expense_by_tag"`
+	SavingsRate       string              `db:"savings_rate" json:"savings_rate"`
+	IncomeRecurring   string              `db:"income_recurring" json:"total_income_recurring"`
+	IncomeOnce        string              `db:"income_once" json:"total_income_once"`
+	TotalIncome       string              `db:"income_total" json:"total_income"`
 }
 
 func getBudgetOverview(c *gin.Context) {
@@ -39,13 +45,13 @@ func getBudgetOverview(c *gin.Context) {
 		return
 	}
 
-	//err = db.Select(&budgetOverview.ExpenseByCategory, "SELECT category, COALESCE(SUM(costs),0) AS total FROM expense WHERE user_id=$1 GROUP BY category ORDER BY total", userID)
-	//if err != nil {
-	//	saveErrorInfo(c, err, 500)
-	//	return
-	//}
+	err = db.Select(&budgetOverview.ExpenseByCategory, "SELECT category, COALESCE(SUM(costs),0) AS total, ROUND(COALESCE(SUM(costs),0)*100 / $2) AS percentage_all, ROUND(COALESCE(SUM(costs),0)*100 / $3) AS percentage_once FROM expense WHERE user_id=$1 GROUP BY category ORDER BY total", userID, "50000", budgetOverview.TotalExpense)
+	if err != nil {
+		saveErrorInfo(c, err, 500)
+		return
+	}
 
-	err = db.Select(&budgetOverview.ExpenseByTag, "SELECT tag_name_costs.tag, COALESCE(SUM(tag_name_costs.costs),0) AS total, ROUND(COALESCE(SUM(tag_name_costs.costs),0)*100 / $2) AS percentage_all, ROUND(COALESCE(SUM(tag_name_costs.costs),0)*100 / $3) AS percentage_once FROM (SELECT tag_id_costs.costs AS costs, tag.name AS tag FROM (SELECT expense.costs AS costs, expense_tag.tag_id AS id FROM expense, expense_tag WHERE expense.id = expense_tag.expense_id AND user_id=$1) AS tag_id_costs, tag WHERE tag_id_costs.id = tag.id) AS tag_name_costs GROUP BY tag_name_costs.tag ORDER BY total", userID, "50000", "9999")
+	err = db.Select(&budgetOverview.ExpenseByTag, "SELECT tag_name_costs.tag, COALESCE(SUM(tag_name_costs.costs),0) AS total, ROUND(COALESCE(SUM(tag_name_costs.costs),0)*100 / $2) AS percentage_all, ROUND(COALESCE(SUM(tag_name_costs.costs),0)*100 / $3) AS percentage_once FROM (SELECT tag_id_costs.costs AS costs, tag.name AS tag FROM (SELECT expense.costs AS costs, expense_tag.tag_id AS id FROM expense, expense_tag WHERE expense.id = expense_tag.expense_id AND user_id=$1) AS tag_id_costs, tag WHERE tag_id_costs.id = tag.id) AS tag_name_costs GROUP BY tag_name_costs.tag ORDER BY total", userID, "50000", budgetOverview.TotalExpense)
 	if err != nil {
 		saveErrorInfo(c, err, 500)
 		return
