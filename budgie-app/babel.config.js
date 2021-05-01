2ee61f04a17c3df0b8836124b266974f2c2daa1e
Daniel Huth <daniel.huth1996@gmail.com>
2021-05-01 18:42:15 +0200

    Add ci app build (#173)

--- budgie-app/babel.config.js
+++ budgie-app/babel.config.js
@@ -2,5 +2,14 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true,
+     }]],
   };
 };
